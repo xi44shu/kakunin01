78b7ae5ac86681b4cb3eb66e814f3408f43595b0
joni-deppu <44shuxichuan@gmail.com>
2022-09-08 20:08:26 +0900

    日付の登録確認

--- db/migrate/20220908103952_create_posts.rb
+++ db/migrate/20220908103952_create_posts.rb
@@ -2,6 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
       t.date :post_date,        null: false
+      t.references :user,            null: false, foreign_key: true
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,10 @@ ActiveRecord::Schema.define(version: 2022_09_08_103952) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "post_date", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -30,4 +32,5 @@ ActiveRecord::Schema.define(version: 2022_09_08_103952) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
