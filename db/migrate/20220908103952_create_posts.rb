88c6417075e71759a112ba8a260771e203524d8c
joni-deppu <44shuxichuan@gmail.com>
2022-09-08 23:37:26 +0900

    content登録確認

--- db/migrate/20220908103952_create_posts.rb
+++ db/migrate/20220908103952_create_posts.rb
@@ -5,6 +5,8 @@ class CreatePosts < ActiveRecord::Migration[6.0]
       t.references :user,            null: false, foreign_key: true
       t.string :start_time,            null: false
       t.string :area,                null: false
+      t.text :content      
+
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2022_09_08_103952) do
     t.bigint "user_id", null: false
     t.string "start_time", null: false
     t.string "area", null: false
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
