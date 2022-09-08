b12395c0b906ab88cbbd6cf8504eb2b7cf0c93df
joni-deppu <44shuxichuan@gmail.com>
2022-09-09 00:20:34 +0900

    detail登録確認

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'posts#index'
   devise_for :users
   resources :posts
+  resources :details
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_08_103952) do
+ActiveRecord::Schema.define(version: 2022_09_08_144139) do
+
+  create_table "details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "construction_method", null: false
+    t.string "pipe_type", null: false
+    t.integer "pipe_size", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.date "post_date", null: false
